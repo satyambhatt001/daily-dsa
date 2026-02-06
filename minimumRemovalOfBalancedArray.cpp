94ab1df1d1e4be8daf04d53d30653696b7fe2411
satyambhatt001 <155147921+satyambhatt001@users.noreply.github.com>
2026-02-06 20:04:20 +0530

    Add minRemoval function to Solution class
    Implement minRemoval function to find minimum removals for balanced array.

---
+++ minimumRemovalOfBalancedArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+     
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int i=0;
+        
+        int maxLen=0;
+        for(int j=0;j<n;j++){
+            while(i<=j && nums[j]>(long long)k*nums[i]){
+              i++;
+            }
+            maxLen=max(maxLen,j-i+1);
+        }
+        return n-maxLen;
+    }
+};

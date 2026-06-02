6f8e7cdf1248fab483f78a1b0dc050c2013cbeab
satyambhatt001 <155147921+satyambhatt001@users.noreply.github.com>
2026-06-02 21:40:19 +0530

    Implement solution for earliest finish time calculation

---
+++ Lc3633.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int>& ls, vector<int>& ld, vector<int>& ws,
+              vector<int>& wd) {
+
+        int mini = INT_MAX;
+
+        for (int i = 0; i < ls.size(); i++) {
+            mini = min(mini, ls[i] + ld[i]);
+        }
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < ws.size(); i++) {
+            ans = min(ans, max(mini, ws[i]) + wd[i]);
+        }
+
+        return ans;
+    }
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+                            return min(
+        solve(landStartTime, landDuration, waterStartTime, waterDuration),
+            solve(waterStartTime, waterDuration, landStartTime, landDuration));
+    }
+};

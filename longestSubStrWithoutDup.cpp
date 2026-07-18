a21114ef413bbebc4c29b1770ab1d2206eba4bb2
satyambhatt001 <155147921+satyambhatt001@users.noreply.github.com>
2026-07-18 15:29:37 +0530

    Implement function to find longest substring without duplicates

---
+++ longestSubStrWithoutDup.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i=0;
+        unordered_map<char,int>mpp;
+        int ans=0;
+       
+        for(int j=0;j<s.size();j++){
+            mpp[s[j]]++;
+          
+
+            while(mpp[s[j]]>1){
+                mpp[s[i]]--;
+                i++;
+            }
+            ans=max(ans,j-i+1);
+
+        }
+        return ans;
+    }
+};
